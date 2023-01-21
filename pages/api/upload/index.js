c72eaa667886205338e88929ea5b91196adf99ab
ChunWarayut <warayut2555@gmail.com>
2023-01-22 02:41:13 +0700

    CREATE UPLOAD FOLDER

--- pages/api/upload/index.js
+++ pages/api/upload/index.js
@@ -12,7 +12,7 @@ export const config = {
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, `pages/api/upload/`)
+        cb(null, `./public/upload/`)
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1])
